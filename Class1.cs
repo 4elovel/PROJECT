92ab91d431fe9d79da6e20f9c4517f58a5a76f30
4elovel <marcynakvova007@gmail.com>
2023-04-21 18:58:30 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Card
+{
+    public Card() { }
+    public Card(int number,char suit) {
+    this.number=number;
+    this.suit = suit;
+    }
+    string skin;
+    char suit { get; }
+    int number { get; }
+}
+
